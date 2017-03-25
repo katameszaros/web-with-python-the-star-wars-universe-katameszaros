4d4e47b67e8ff168560672bad428b5d203f55fb9
Kata Meszaros <kata.meszaros92@gmail.com>
2017-03-25 19:36:33 +0100

    Ajax works for one person/planet only

--- static/main.js
+++ static/main.js
@@ -9,7 +9,7 @@
                 if (planets[i].residents.length === 0){
                     return 'No known residents';
                 }else{
-                    return '<button>' +  planets[i].residents.length + ' residents </button>'
+                    return '<button id=' +i +' type="button" data-toggle="modal" data-target="#myModal">' +  planets[i].residents.length + ' residents </button>'
                     }
                 };
             for (var i = 0; i < planets.length; i++) {
@@ -22,7 +22,48 @@
                     '<td>' + planets[i].population + '</td>' +
                     '<td>' + CheckResidents(i) + '</td></tr>'
             )}
-        }
+            $('button').click(function(){
+                for (var i = 0; i < planets.length; i++) {
+                    if (parseInt(this.id) === i){
+                        for (var j = 0; j < planets[i].residents.length; j++) {
+                            console.log(planets[i].residents[j]);
+                                var request_for_people = new XMLHttpRequest();
+                                request_for_people.open('GET', planets[i].residents[j], true);
+                                request_for_people.onload = function () {
+                                if (request_for_people.status >= 200 && request_for_people.status < 400) {
+                                    var people_data = JSON.parse(request_for_people.responseText);
+                                    console.log(people_data);
+                                    $("#peopledata").append(
+                                        '<tr class="individual_data"><td>' + people_data.name + '</td>' +
+                                        '<td>' + people_data.height + '</td>' +
+                                        '<td>' + people_data.mass + '</td>' +
+                                        '<td>' + people_data.skin_color + '</td>'+
+                                        '<td>' + people_data.hair_color + '</td>'+
+                                        '<td>' + people_data.birth_year + '</td>'+
+                                        '<td>' + people_data.gender + '</td></tr>'
+                                    )
+
+                                }
+                            };
+
+                        };
+
+                        request_for_people.send();
+
+
+                        $('.modal-header').append(
+                            '<h4 class="modal-title"> Residents of ' + planets[i].name + '</h4>'
+                        )
+
+                    }
+                }
+            });
+
+            $('#close_modal').click(function(){
+                $('.modal-title').remove();
+                $('.individual_data').remove();
+            });
+        };
     };
 
     request.send();

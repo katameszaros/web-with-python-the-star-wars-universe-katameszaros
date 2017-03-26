8cd9145b1b40deef241f9266f8981366908c48e2
Kata Meszaros <kata.meszaros92@gmail.com>
2017-03-26 14:34:50 +0200

    Next and previous page feature implemented, refactored main.js

--- static/main.js
+++ static/main.js
@@ -1,69 +1,124 @@
+function loadPlanetPageData(urlOfPage) {
     var request = new XMLHttpRequest();
-    request.open('GET', 'http://swapi.co/api/planets/', true);
+    request.open('GET', urlOfPage, true);
     request.onload = function () {
-        if (request.status >= 200 && request.status < 400) {
-            var data = JSON.parse(request.responseText);
+        if (this.status >= 200 && this.status < 400) {
+            var data = JSON.parse(this.responseText);
             var planets = data['results'];
-            function CheckResidents(i){
-                if (planets[i].residents.length === 0){
-                    return 'No known residents';
-                }else{
-                    return '<button id=' +i +' type="button" data-toggle="modal" data-target="#myModal">' +  planets[i].residents.length + ' residents </button>'
-                    }
-                };
-            for (var i = 0; i < planets.length; i++) {
-                $("#planetdata").append(
-                    '<tr><td>' + planets[i].name + '</td>' +
-                    '<td>' + planets[i].diameter / 1000 + ' km </td>' +
-                    '<td>' + planets[i].climate + '</td>' +
-                    '<td>' + planets[i].terrain + '</td>' +
-                    '<td>' + planets[i].surface_water + ' % </td>' +
-                    '<td>' + planets[i].population + '</td>' +
-                    '<td>' + CheckResidents(i) + '</td></tr>'
-            )}
-            $('button').click(function(){
-                for (var i = 0; i < planets.length; i++) {
-                    if (parseInt(this.id) === i){
-                        for (var j = 0; j < planets[i].residents.length; j++) {
-                            console.log(planets[i].residents[j]);
-                                var request_for_people = new XMLHttpRequest();
-                                request_for_people.open('GET', planets[i].residents[j], true);
-                                request_for_people.onload = function () {
-                                    if (this.status >= 200 && this.status < 400) {
-                                        var people_data = JSON.parse(this.responseText);
-                                        console.log(people_data);
-                                        $("#peopledata").append(
-                                            '<tr class="individual_data"><td>' + people_data.name + '</td>' +
-                                            '<td>' + people_data.height + '</td>' +
-                                            '<td>' + people_data.mass + '</td>' +
-                                            '<td>' + people_data.skin_color + '</td>'+
-                                            '<td>' + people_data.hair_color + '</td>'+
-                                            '<td>' + people_data.birth_year + '</td>'+
-                                            '<td>' + people_data.gender + '</td></tr>'
-                                        );
 
+            bindNavButton("#next", data.next);
+            bindNavButton("#previous", data.previous);
+            bindPlanetTable(planets);
 
-                                    }
-
-                                };
-                                request_for_people.send();
-                        };
-
-
-
-                        $('.modal-header').append(
-                            '<h4 class="modal-title"> Residents of ' + planets[i].name + '</h4>'
-                        );
-
-                    }
-                }
+            $('.get_people_data').click(function () {
+                clearResidentData();
+                var planet = planets[parseInt(this.id)];
+                loadResidentData(planet);
+                bindModalTitle(planet.name);
             });
-
-            $('#close_modal').click(function(){
-                $('.modal-title').remove();
-                $('.individual_data').remove();
-            });
-        };
+        }
     };
 
     request.send();
+}
+
+function bindNavButton(buttonSelector, data) {
+    $(buttonSelector).unbind();
+    if (data !== null) {
+        $(buttonSelector).prop("disabled", false);
+        $(buttonSelector).click(function () {
+            $(".planet_data").remove();
+            loadPlanetPageData(data);
+        });
+    } else {
+        $(buttonSelector).prop("disabled", true);
+    }
+}
+
+function bindPlanetTable(planets) {
+    for (var i = 0; i < planets.length; i++) {
+        $("#planetdata").append(
+            '<tr class="planet_data"><td>' + planets[i].name + '</td>' +
+            '<td>' + checkIfNumeric(planets[i].diameter / 1000, " km") + '</td>' +
+            '<td>' + planets[i].climate + '</td>' +
+            '<td>' + planets[i].terrain + '</td>' +
+            '<td>' + checkIfNumeric(planets[i].surface_water, " %") + '</td>' +
+            '<td>' + checkIfNumeric(planets[i].population, " people") + '</td>' +
+            '<td>' + checkResidents(planets[i], i) + '</td></tr>'
+        )
+    }
+}
+
+function bindLineOfIndividual(people_data) {
+    $("#peopledata").append(
+        '<tr class="individual_data"><td>' + people_data.name + '</td>' +
+        '<td>' + people_data.height / 100 + ' m</td>' +
+        '<td>' + checkIfNumeric(people_data.mass, " kg") + '</td>' +
+        '<td>' + people_data.skin_color + '</td>' +
+        '<td>' + checkIfNone(people_data.hair_color) + '</td>' +
+        '<td>' + people_data.birth_year + '</td>' +
+        '<td>' + people_data.gender + '</td></tr>'
+    );
+}
+
+
+function clearResidentData() {
+    $('.modal-title').remove();
+    $('.individual_data').remove();
+}
+
+
+function loadResidentData(planet) {
+    for (var j = 0; j < planet.residents.length; j++) {
+        var request_for_people = new XMLHttpRequest();
+        request_for_people.open('GET', planet.residents[j], true);
+        request_for_people.onload = function () {
+            if (this.status >= 200 && this.status < 400) {
+                var people_data = JSON.parse(this.responseText);
+                bindLineOfIndividual(people_data);
+            }
+        };
+        request_for_people.send();
+    }
+}
+
+function bindModalTitle(planet_name) {
+    $('.modal-header').append(
+        '<h4 class="modal-title"> Residents of ' + planet_name + '</h4>'
+    );
+}
+
+
+function formatNumber(x) {
+    if (isNaN(x))return "";
+    n = x.toString().split('.');
+    return n[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",") + (n.length > 1 ? "." + n[1] : "");
+}
+
+
+function checkResidents(planet, index) {
+    if (planet.residents.length === 0) {
+        return 'No known residents';
+    } else {
+        return '<button id=' + index + ' class = "get_people_data" type="button" data-toggle="modal" data-target="#myModal">' + planet.residents.length + ' residents </button>'
+    }
+}
+
+function checkIfNone(data) {
+    if (data === "none") {
+        return "unknown";
+    } else {
+        return data;
+    }
+}
+
+function checkIfNumeric(data, addOn) {
+    if ($.isNumeric(data)) {
+        return formatNumber(data) + addOn;
+    } else {
+        return "unknown";
+    }
+}
+
+
+loadPlanetPageData('http://swapi.co/api/planets/');
